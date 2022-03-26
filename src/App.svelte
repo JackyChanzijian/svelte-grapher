3ba7adad00c12ca6bd3a9022b1de454aa9d8a3a2
Jacky Chan <1303213163@qq.com>
2022-03-26 21:44:03 +0800

    process input

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,29 @@
 	import Sheet from "./Sheet.svelte";
 
 	let datas = [];
+	
+	let dataValue = 0;
+	let dataName = "";
+
+	$: console.table(datas);
+
+	// Handle input
+	function addData() {
+		datas = [{name: dataName, value: dataValue}, ...datas];	// Add input field to datas array
+		//Clear input field
+		dataValue = 0;
+		dataName = "";	
+	}
+
 </script>
 
 <main>
 	<BarGraph datas={datas} />
 	<Sheet datas={datas} />
 	<form>
-		<input type="text">
-		<input type="number">
-		<input type="submit" value="Enter">
+		<input type="text" bind:value={dataName}>
+		<input type="number" bind:value={dataValue}>
+		<input type="button" value="Enter" on:click={addData}>
 	</form>
 </main>
 
